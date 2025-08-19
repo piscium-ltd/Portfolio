cc900216fc34162619347f5f25ed630edabdf80c
PhilomenaMusili <philomenamusili2@gmail.com>
2025-08-20 01:19:24 +0300

    fix: updated service page

--- showcase/views.py
+++ showcase/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+from django.conf import settings
 
 def index(request):
     return render(request, 'index.html')
--- src/settings.py
+++ src/settings.py
@@ -126,3 +126,13 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email settings for your domain email
+# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+# EMAIL_HOST = "mail.janjaprogrammers.com"
+# EMAIL_PORT = 465  
+# EMAIL_USE_SSL = True  
+# EMAIL_USE_TLS = False 
+# EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER") 
+# EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
